c82cd6c48ce7faecdeaa974649c08dab7d97192e
Aidan Baros <aidanlhbaros@gmail.com>
2023-11-20 19:01:06 -0600

    User done and other small changes

--- src/Database.java
+++ src/Database.java
@@ -2,29 +2,31 @@ import java.util.ArrayList;
 
 public class Database {
     protected ArrayList<User> listofUsers;
+    private static int IDcounter = 0;
 
     public Database(){
         listofUsers = new ArrayList<User>();
     }
 
+    // adds a new user to the database
     public boolean addUser(String name){
-        // need to ask the prof about how to create IDs with static again
         if(name != null){
-            listofUsers.add(new User(name,1));
-            return true;
+            listofUsers.add(new User(name,IDcounter++));
+            return true; // returns true if it was able to add a user to the database
         }
-        //else making it unknown
         else{
-            return false;
+            return false; //returns false if it did not add the user to the data base
         }
     }
 
+    // removes a user from the database based on the users name
     public boolean removeUser(String name){
         // This needs to have an interface with it so that you can see the playlist assocated to the name so that is there is the same name twice you can chose with name to delete
         
         return true;
     }
 
+    // removes a user from the database based on theie ID
     public boolean removeUser(int ID){
         for (User user:listofUsers){
             if (user.ID == ID){
--- src/User.java
+++ src/User.java
@@ -1,45 +1,72 @@
 public class User {
     protected String name;
     protected int ID;
+    protected Playlist playlist;
 
     public User(String newName, int newID){
         name = newName;
         ID = newID;
+        playlist = new Playlist();
     }
 
-    public void addRecording(){
-
+    public void addRecording(Recording newRecording){
+        playlist.addRecording(newRecording);
     }
 
-    public void importPlaylist(){
-
+    public boolean addRecording(String type, String name, String artist, int duration, double rate){
+        if(type.equals("V")){
+            playlist.addRecording(new Video(name,artist,duration,rate));
+            return true;
+        }
+        else if (type.equals("A")){
+            playlist.addRecording(new Audio(name,artist,duration,rate));
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 
-    public void copyPlaylist(){
-
+    public boolean importPlaylist(String File){
+        return playlist.addMultipleRecordings(File);
     }
 
-    public void reomveRecording(){
-
+    public void copyPlaylist(Playlist otherPlaylist){
+        playlist.addMultipleRecordings(otherPlaylist);
     }
 
-    public void playRecording(){
+    public boolean reomveRecording(String name){
+        return playlist.removeRecording(name);
+    }
 
+    public boolean reomveRecording(int index){
+        return playlist.removeRecording(index);
+    }
+
+    public void playRecording(String name){
+        playlist.playRecording(name);
+    }
+
+    public void playRecording(int ID){
+        playlist.playRecording(ID);
     }
 
     public void playPlaylist(){
-
+        playlist.play();
     }
 
     public void shuffle(){
-
+        playlist.shuffle();
     }
 
-    public void exportPlaylist(){
-
+    public boolean exportPlaylist(){
+        return playlist.save();
     }
 
     public void stats(){
-        
+        System.out.println("Username:" + name);
+        System.out.println("ID:" + ID);
+        System.out.println("Playlist details:" + name);
+        playlist.stats();
     }
 }
