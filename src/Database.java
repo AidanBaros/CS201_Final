b98a10651c5decddc51cad1e73750ea03b566153
Aidan Baros <aidanlhbaros@gmail.com>
2023-11-20 19:25:47 -0600

    database should be done

--- src/Database.java
+++ src/Database.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Database {
     protected ArrayList<User> listofUsers;
@@ -22,8 +24,41 @@ public class Database {
     // removes a user from the database based on the users name
     public boolean removeUser(String name){
         // This needs to have an interface with it so that you can see the playlist assocated to the name so that is there is the same name twice you can chose with name to delete
-        
-        return true;
+        // done
+        ArrayList <User> usersWithName = new ArrayList<User>();
+        for (User user:listofUsers){
+            if (user.name.equals(name)){
+                usersWithName.add(user);
+            }
+        }
+        if (usersWithName.size() == 0){
+            System.out.println("No User by that name");
+            return false;
+        }
+        else if (usersWithName.size() == 1){
+            listofUsers.remove(usersWithName.get(0));
+            return true;
+        }
+        else{
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Which user would you like to remove? ");
+            int counter = 1;
+            for (User user:usersWithName){
+                System.out.println("User #" + counter++);
+                user.stats();
+            }
+            System.out.print(" -- ");
+            try{
+                int whichUser = scanner.nextInt();
+                listofUsers.remove(usersWithName.get(whichUser - 1));
+                scanner.close();
+            return true;
+            }catch(InputMismatchException ex){
+                System.out.println("Not an option");
+                scanner.close();
+                return false;
+            }
+        }
     }
 
     // removes a user from the database based on theie ID
