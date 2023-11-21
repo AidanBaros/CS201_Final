00a09097a4a831899a06f0e443c59d7f699d8df0
Aidan2426 <144728598+Aidan2426@users.noreply.github.com>
2023-11-21 14:23:33 -0600

    Add files via upload
    Not sure if this is exactly right

--- src/Interface.java
+++ src/Interface.java
@@ -1,3 +1,32 @@
-public class Interface {
+
+public class Interface
+{
+
+    int state = 0;
+
+    public Interface(int state)
+    {
+        this.state  = state;
+    }
+
+    public void setState(int state)
+    {
+        this.state = state;
+    }
     
-}
+    public int getState()
+    {
+        return this.state;
+    }
+
+    public void choose(int state)
+    {
+        if(state == 0){
+            System.out.println("This the music player");
+            System.out.println("Options List");
+            System.out.println("1 : Add User");
+            System.out.println("2: Remove User");
+            System.out.println("3: List Users");
+            System.out.println("4: Choose User");
+        }
+    }
